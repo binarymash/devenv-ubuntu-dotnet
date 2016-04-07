13b49d28094b7f8d1d4b76774ba17b7241dfe316
Philip Wood <philip.m.wood@gmail.com>
2016-04-07 21:03:45 +0100

    added git, gulp, npm, yeoman

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # devenv-ubuntu-dotnet
 Vagrant box with Ubuntu 14.04 Desktop and .net core
+
+# Requirements
+
+Vagrant
+
+# What's installed
+
+.net core
+git
+gulp
+npm
+yeoman
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
-#install apt-get feed for dotnet - see https://dotnet.github.io/getting-started/
+#dotnet apt-get feed - see https://dotnet.github.io/getting-started/
 sudo sh -c 'echo "deb [arch=amd64] http://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 sudo apt-get update
 
-#install dotnet
+#dotnet
 sudo apt-get --yes install dotnet=1.0.0.001598-1
+
+#npm
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+
+#git
+sudo apt-get install -y git
+
+#packages from npm
+npm install -g gulp-cli yo
+
+#npm install -g generator-aspnet
+
+#create dev folder and link from vagrant
+[ -d "/vagrant/dev" ] && echo "/vagrant/dev already exists, so we won't recreate it" || mkdir /vagrant/dev
+ln -s /vagrant/dev ~/dev
