349fd8a7334543cf684013413b0e44870dcbd868
Philip Wood <philip.m.wood@gmail.com>
2016-04-15 07:23:32 +0100

    added vscode 1.0, aspen generator for yeoman

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,15 +11,17 @@ sudo apt-get --yes install dotnet=1.0.0.001598-1
 #npm
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+#sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules}
 
 #git
 sudo apt-get install -y git
 
 #packages from npm
-npm install -g gulp-cli yo
+npm install -g gulp-cli yo generator-aspnet
 
-#npm install -g generator-aspnet
+#vscode 1.0
+URL='https://az764295.vo.msecnd.net/stable/fa6d0f03813dfb9df4589c30121e9fcffa8a8ec8/vscode-amd64.deb'; FILE='vscodetemp';  wget "$URL" -O$FILE && sudo dpkg -i $FILE; rm $FILE
 
 #create dev folder and link from vagrant
 [ -d "/vagrant/dev" ] && echo "/vagrant/dev already exists, so we won't recreate it" || mkdir /vagrant/dev
