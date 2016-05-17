456cf795a3de078bb8710b03f10c486a11e04bd5
Philip Wood <philip.m.wood@gmail.com>
2016-05-17 21:13:59 +0100

    Updated to .net core SDK preview 1, .net core RC2, Visual Studio Code 1.1. Improved readme

--- README.md
+++ README.md
@@ -1,14 +1,44 @@
-# devenv-ubuntu-dotnet
-Vagrant box with Ubuntu 14.04 Desktop and .net core
+##devenv-ubuntu-dotnet
 
-# Requirements
+This is a Vagrant box with Ubuntu 14.04 Desktop, .net core, Visual Studio Code
+ and several other development packages useful for developing web based applications
 
-Vagrant
+##Requirements
 
-# What's installed
+The following tools will need to be installed on your machine...
 
-.net core
-git
-gulp
-npm
-yeoman
+* Vagrant
+* VirtualBox
+
+##Installation
+
+Run the following command line...
+
+    vagrant up
+
+####Manual steps
+Install the C# extension to Visual Studio Code
+
+* Visual Studio Code > View > Command Palette
+* ext install C#
+
+##What's installed
+
+* .NET core SDK - *preview 1 (dotnet-dev-1.0.0-preview1-002702)*
+* .NET core - *RC2 (dotnet-sharedframework-microsoft.netcore.app-1.0.0-rc2-3002702)*
+* Visual Studio Code - *1.1*
+* git
+* gulp
+* npm
+* yeoman
+
+##Getting started
+
+For the simplest .NET core application...
+
+    $ cd /vagrant/dev
+    $ mkdir helloWorld
+    $ cd helloWorld
+    $ dotnet new
+    $ dotnet restore
+    $ dotnet run
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 #dotnet apt-get feed - see https://dotnet.github.io/getting-started/
-sudo sh -c 'echo "deb [arch=amd64] http://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
-sudo apt-get update
+#sudo sh -c 'echo "deb [arch=amd64] http://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+#sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+#sudo apt-get update
 
 #dotnet
-sudo apt-get --yes install dotnet=1.0.0.001598-1
+#sudo apt-get --yes install dotnet=1.0.0-preview1-002702
+
+sudo apt-get update
+
+sudo apt-get install libunwind8
+
+curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash /dev/stdin --version 1.0.0-preview1-002702 --install-dir ~vagrant/dotnet
+
+sudo ln -s ~vagrant/dotnet/dotnet /usr/local/bin
 
 #npm
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
@@ -20,8 +28,8 @@ sudo apt-get install -y git
 #packages from npm
 npm install -g gulp-cli yo generator-aspnet
 
-#vscode 1.0
-URL='https://az764295.vo.msecnd.net/stable/fa6d0f03813dfb9df4589c30121e9fcffa8a8ec8/vscode-amd64.deb'; FILE='vscodetemp';  wget "$URL" -O$FILE && sudo dpkg -i $FILE; rm $FILE
+#vscode 1.1
+URL='https://go.microsoft.com/fwlink/?LinkID=760868'; FILE='vscodetemp';  wget "$URL" -O$FILE && sudo dpkg -i $FILE; rm $FILE
 
 #create dev folder and link from vagrant
 [ -d "/vagrant/dev" ] && echo "/vagrant/dev already exists, so we won't recreate it" || mkdir /vagrant/dev
